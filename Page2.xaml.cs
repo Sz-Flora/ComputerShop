b4486ff1734d04936da4acdb1031057902e1bbbe
SzFlora <szabof@kkszki.hu>
2025-09-29 12:25:29 +0200

    Regisztráció is ready

---
+++ Page2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ComputerShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComputerShop
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        IDatabase _database = new Users();
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void regButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_database.GetData(usernameTextBox.Text, passwordTextBox.Password, emailTextBox.Text, fullnameTextBox.Text).ToString());
+        }
+    }
+}
--- Services/Users.cs
+++ Services/Users.cs
@@ -11,6 +11,26 @@ namespace ComputerShop.Services
     internal class Users : IDatabase
     {
         Connect conn = new Connect();
+
+        public object AddRecord(string username, string password, string email, string fullname)
+        {
+            conn._connection.Open();
+
+            string sql = "INSERT INTO `users` (`UserName`, `FullName`, `Password`, `Email`) VALUES (@username, @fullname, @password, @email)";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn._connection);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@fullname", fullname);
+            cmd.Parameters.AddWithValue("@password", password);
+            cmd.Parameters.AddWithValue("@email", email);
+
+            cmd.ExecuteNonQuery();
+
+            conn._connection.Close();
+
+            return new { message = "Sikeres regisztráció!" };
+        }
+
         public ICollection<object> GetAllData()
         {
             ICollection<object> data = new List<object>();
