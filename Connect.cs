87559135d6006246b1993128fb73525b5582efc9
SzFlora <szabof@kkszki.hu>
2025-09-29 10:25:33 +0200

    Connect.cs is ready

--- Connect.cs
+++ Connect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +8,26 @@ using System.Threading.Tasks;
 namespace ComputerShop
 {
     internal class Connect
-    { 
+    {
+        public MySqlConnection _connection;
+
         private string _host;
         private string _db;
         private string _user;
         private string _pass;
+
+        private string _connectionString;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "computershop";
+            _user = "root";
+            _pass = "";
+
+            _connectionString = $"SERVER={_host};DATABASE={_db};UID={_user};PASSWORD={_pass};SslMode=None";
+
+            _connection = new MySqlConnection(_connectionString);
+        }
     }
 }
